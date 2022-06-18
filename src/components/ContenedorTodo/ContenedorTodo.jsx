da527846ae89e1dfc72767832eefc1579af98a7e
Jose Miguel Alcaraz <90214208+jose1up@users.noreply.github.com>
2022-06-18 03:33:26 -0300

    fixing css

--- src/components/ContenedorTodo/ContenedorTodo.jsx
+++ src/components/ContenedorTodo/ContenedorTodo.jsx
@@ -3,7 +3,14 @@ import icons_success from "../../../assets/succes.png";
 import icons_remove from "../../../assets/remove.png";
 import icons_reset from "../../../assets/reset.png";
 import s from "./ContenedorTodo.module.css";
-export const ContenedorTodo = ({ id, task_name, status, removed, success, reset }) => {
+export const ContenedorTodo = ({
+  id,
+  task_name,
+  status,
+  removed,
+  success,
+  reset,
+}) => {
   return (
     <div
       className={
@@ -14,13 +21,28 @@ export const ContenedorTodo = ({ id, task_name, status, removed, success, reset 
     >
       <span className={s.spanIcon}>{task_name}</span>
       <div className={s.divIcon}>
-        {status =="completed" ? (
-          <img src={icons_reset} alt="icons reset" onClick={reset} />
+        {status == "completed" ? (
+          <img
+            src={icons_reset}
+            alt="icons reset"
+            onClick={reset}
+            className={s.img}
+          />
         ) : (
-          <img src={icons_success} alt="icons succes" onClick={success} />
+          <img
+            src={icons_success}
+            alt="icons succes"
+            onClick={success}
+            className={s.img}
+          />
         )}
 
-        <img src={icons_remove} alt="icons remove" onClick={removed} />
+        <img
+          src={icons_remove}
+          alt="icons remove"
+          onClick={removed}
+          className={s.img}
+        />
       </div>
     </div>
   );
